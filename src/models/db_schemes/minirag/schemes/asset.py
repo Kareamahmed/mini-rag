86ae3bd0ca07d3811b40efdc5b1112be7fa399ce
Kareamahmed <kareama47@gmail.com>
2026-08-09 22:35:58 +0300

    Add initial database schema models for Asset, DataChunk, Project, and base class

---
+++ src/models/db_schemes/minirag/schemes/asset.py
@@ -0,0 +1,38 @@
+from .minirag_base import SQLAlchemyBase
+from sqlalchemy import Column, Integer, DateTime, func, String, ForeignKey, Index
+from sqlalchemy.dialects.postgresql import UUID, JSONB
+from sqlalchemy.orm import relationship
+import uuid
+
+
+class Asset(SQLAlchemyBase):
+
+    __tablename__ = "assets"
+
+    asset_id = Column(Integer, primary_key=True, autoincrement=True)
+    asset_uuid = Column(UUID, default=uuid.uuid4, nullable=False, unique=True)
+
+    asset_name = Column(String, nullable=False)
+    asset_type = Column(String, nullable=False)
+    asset_size = Column(Integer, nullable=False)
+    asset_created_at = Column(
+        DateTime(timezone=True), server_default=True, nullable=False
+    )
+    asset_config = Column(JSONB, nullable=True)
+
+    asset_project_id = Column(
+        Integer, ForeignKey("projects.project_id"), nullable=False
+    )
+
+    project = relationship(
+        "Project", back_populates="assets"
+    )  # many assets in one project
+
+    __table_args__ = (Index("ix_asset_project_id", asset_project_id))
+
+
+# JSON
+# → textual JSON representation
+
+# JSONB
+# → decomposed binary representation
---
+++ src/models/db_schemes/minirag/schemes/minirag_base.py
@@ -0,0 +1,4 @@
+from sqlalchemy.ext.declarative import declarative_base
+
+# Create a base class for all SQLAlchemy ORM models
+SQLAlchemyBase = declarative_base()
